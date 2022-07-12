ddb6cd3093c0c7b2883c1517fae3e4ff9503bee8
Emre1485 <97231294+Emre1485@users.noreply.github.com>
2022-07-12 12:32:59 +0300

    Add files via upload

---
+++ CinZodyak.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class CinZodyak {
+    public static void main(String[] args) {
+        int dogumTarihi, cinZodyak;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Dogum Yilinizi Giriniz: ");
+        dogumTarihi = inp.nextInt();
+
+        cinZodyak = dogumTarihi % 12;
+        switch (cinZodyak){
+            case 0:
+                System.out.println("Cin Zodyagi Burcunuz: Maymun");
+                break;
+            case 1:
+                System.out.println("Cin Zodyagi Burcunuz: Horoz");
+                break;
+            case 2:
+                System.out.println("Cin Zodyagi Burcunuz: Kopek");
+                break;
+            case 3:
+                System.out.println("Cin Zodyagi Burcunuz: Domuz");
+                break;
+            case 4:
+                System.out.println("Cin Zodyagi Burcunuz: Fare");
+                break;
+            case 5:
+                System.out.println("Cin Zodyagi Burcunuz: Okuz");
+                break;
+            case 6:
+                System.out.println("Cin Zodyagi Burcunuz: Kaplan");
+                break;
+            case 7:
+                System.out.println("Cin Zodyagi Burcunuz: Tavsan");
+                break;
+            case 8:
+                System.out.println("Cin Zodyagi Burcunuz: Ejderha");
+                break;
+            case 9:
+                System.out.println("Cin Zodyagi Burcunuz: Yilan");
+                break;
+            case 10:
+                System.out.println("Cin Zodyagi Burcunuz: At");
+                break;
+            case 11:
+                System.out.println("Cin Zodyagi Burcunuz: Koyun");
+                break;
+        }
+    }
+}
